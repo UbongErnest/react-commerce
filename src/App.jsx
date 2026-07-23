1b51ce982837bd78342d495756573e18547a6d05
UbongErnest <ubongudoekpoernest@gmail.com>
2026-07-23 20:47:07 +0100

    Added style

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../src/pages/index.css'
+import HomePage from './pages/HomePage'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
