00166d0754790c60683fb83e8085faa8a977d780
UbongErnest <ubongudoekpoernest@gmail.com>
2026-07-23 21:39:09 +0100

    Added trackings page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from './pages/HomePage'
 import './App.css'
 import { Route, Routes } from 'react-router'
 import CheckoutPage from './pages/CheckoutPage'
+import Orders from './pages/Orders'
+import Trackings from './pages/Trackings'
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="checkout" element={<CheckoutPage/>}/>
+      <Route path="orders" element = {<Orders/>}/>
+      <Route path="tracking" element = {<Trackings/>}/>
     </Routes>
       
     </>
